ced0ee7e81cb871b22e88060024baa1011852509
Jenn J Will <jennijwill@gmail.com>
2019-07-29 13:57:34 -0700

    testing checkpoint completed

---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,18 @@
+const MenuController = require("../controllers/MenuController");
+
+describe("MenuController", () => {
+  //MenuController is passed as name of the test suite
+  beforeEach(() => {
+    this.menu = new MenuController();
+  });
+  describe("#getContactCount()", () => {
+    it("should return 0 when no contacts are in the book", () => {
+      //function implementing the specs
+      expect(this.menu.getContactCount()).toBe(0); //toBe is a matcher in Jasmine
+    });
+    it("should return 1 when there is exactly one contact in the book", () => {
+      this.menu.contacts.push("Bob");
+      expect(this.menu.getContactCount()).toBe(1);
+    });
+  });
+});
